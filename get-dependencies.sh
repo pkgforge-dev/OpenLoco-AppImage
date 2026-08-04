884d2d0792140649155eb97a4f3d3f76ca2e7f34
Link4Electronics <link4electronics@gmail.com>
2026-08-04 19:55:57 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,118 +31,12 @@ get-debloated-pkgs --add-common --prefer-nano
 #pacman -Q "$package" | awk '{print $2; exit}' > ~/version
 
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
-cat <<'EOM' > "PKGBUILD"
-pkgname=openloco-git
-pkgver=26.02.r13.g033022c0f
-pkgrel=1
-pkgdesc="An open source re-implementation of Chris Sawyer's Locomotion"
-arch=(x86_64 aarch64)
-url="https://github.com/OpenLoco/OpenLoco"
-license=(MIT)
-depends=(sdl3 libpng openal)
-makedepends=(cmake yaml-cpp git gtest fmt)
-provides=(openloco)
-conflicts=(openloco)
-options=(lto !debug)
-source=("git+https://github.com/OpenLoco/OpenLoco.git")
-sha256sums=('SKIP')
-pkgver() {
-  cd OpenLoco
-  git describe --long --tags | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g'
-}
-build() {
-	local _flags=(
-    -DFETCHCONTENT_QUIET:BOOL=OFF \
-    -DOPENLOCO_BUILD_TESTS=OFF \
-    -DCMAKE_CXX_FLAGS="-Wno-error" \
-	)
-  cd "${srcdir}/OpenLoco"
-  rm -rf build
-  cmake -G "Unix Makefiles" -B build -S . -Wno-dev \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_INSTALL_DATAROOTDIR=/usr/share \
-    -DCMAKE_INSTALL_DATADIR=/usr/share/data \
-    -DUSE_SYSTEM_FMT=ON \
-    -DCMAKE_SIZEOF_VOID_P=8 \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    "${_flags[@]}"
-  cmake --build build
-}
-package() {
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_x16.png" "$pkgdir/usr/share/icons/hicolor/16x16/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_x32.png" "$pkgdir/usr/share/icons/hicolor/32x32/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_x64.png" "$pkgdir/usr/share/icons/hicolor/64x64/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_x128.png" "$pkgdir/usr/share/icons/hicolor/128x128/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_x256.png" "$pkgdir/usr/share/icons/hicolor/256x256/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_x512.png" "$pkgdir/usr/share/icons/hicolor/512x512/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco/src/Resources/src/logo/icon_steam.svg" "$pkgdir/usr/share/icons/hicolor/scalable/apps/openloco.svg"
-  cd "${srcdir}/OpenLoco"
-  DESTDIR="${pkgdir}" cmake --install build
-  install -D "${srcdir}/OpenLoco/LICENSE" -t "${pkgdir}/usr/share/licenses/${pkgname}"
-  rm -rf "${pkgdir}/usr/lib/libfmt.a"
-  rm -rf "${pkgdir}/usr/include/fmt"
-  rm -rf "${pkgdir}/usr/lib/cmake/fmt"
-  rm -rf "${pkgdir}/usr/lib/pkgconfig/fmt.pc"
-  rm -rf "${pkgdir}/usr/share/include/sfl"
-}
-EOM
-make-aur-package
-pacman -Q "openloco-git" | awk '{print $2; exit}' > ~/version
+	package=openloco-git
 else
-cat <<'EOM' > "PKGBUILD"
-pkgname=openloco
-pkgver=25.12
-pkgrel=1
-pkgdesc="An open source re-implementation of Chris Sawyer's Locomotion"
-arch=(x86_64 aarch64)
-url="https://github.com/OpenLoco/OpenLoco"
-license=(MIT)
-depends=(sdl2 libpng openal)
-makedepends=(cmake yaml-cpp gtest fmt git)
-options=(lto !debug)
-source=("${pkgname}-${pkgver}.tar.gz::https://github.com/OpenLoco/OpenLoco/archive/refs/tags/v${pkgver}.tar.gz")
-sha256sums=('11e06a365c083940665cfeaa0c367686b9171733cd05bb7692222226b74a716e')
-build() {
-  local _flags=(
-    -DFETCHCONTENT_QUIET:BOOL=OFF
-    -DOPENLOCO_BUILD_TESTS=OFF
-    -DCMAKE_CXX_FLAGS="-Wno-error"
-  )
-  cd "${srcdir}/OpenLoco-${pkgver}"
-  rm -rf build
-  cmake -G "Unix Makefiles" -B build -S . -Wno-dev \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_INSTALL_DATAROOTDIR=/usr/share \
-    -DCMAKE_INSTALL_DATADIR=/usr/share/data \
-    -DUSE_SYSTEM_FMT=ON \
-    -DCMAKE_SIZEOF_VOID_P=8 \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    "${_flags[@]}"
-  cmake --build build
-}
-package() {
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_x16.png" "$pkgdir/usr/share/icons/hicolor/16x16/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_x32.png" "$pkgdir/usr/share/icons/hicolor/32x32/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_x64.png" "$pkgdir/usr/share/icons/hicolor/64x64/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_x128.png" "$pkgdir/usr/share/icons/hicolor/128x128/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_x256.png" "$pkgdir/usr/share/icons/hicolor/256x256/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_x512.png" "$pkgdir/usr/share/icons/hicolor/512x512/apps/openloco.png"
-  install -Dm644 "${srcdir}/OpenLoco-${pkgver}/src/Resources/src/logo/icon_steam.svg" "$pkgdir/usr/share/icons/hicolor/scalable/apps/openloco.svg"
-  cd "${srcdir}/OpenLoco-${pkgver}"
-  DESTDIR="${pkgdir}" cmake --install build
-  install -D "${srcdir}/OpenLoco-${pkgver}/LICENSE" -t "${pkgdir}/usr/share/licenses/${pkgname}"
-  rm -rf "${pkgdir}/usr/lib/libfmt.a"
-  rm -rf "${pkgdir}/usr/include/fmt"
-  rm -rf "${pkgdir}/usr/lib/cmake/fmt"
-  rm -rf "${pkgdir}/usr/lib/pkgconfig/fmt.pc"
-  rm -rf "${pkgdir}/usr/share/include/sfl"
-}
-EOM
-make-aur-package
-pacman -Q "openloco" | awk '{print $2; exit}' > ~/version
+	package=openloco
 fi
+make-aur-package "$package"
+pacman -Q "$package" | awk '{print $2; exit}' > ~/version
 
 mkdir -p ./AppDir/bin
 mv -v /usr/bin/data ./AppDir/bin
