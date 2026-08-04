cabb39e2b8942bec0a5bfc98b93ac7e27bf57d61
Link4Electronics <link4electronics@gmail.com>
2026-08-04 20:03:49 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,14 +16,6 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
-#if [ "${DEVEL_RELEASE-}" = 1 ]; then
-#	package=openloco-git
-#else
-#	package=openloco
-#fi
-#make-aur-package "$package"
-#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
-
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	package=openloco-git
 else
