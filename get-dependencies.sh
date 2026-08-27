79812ad32ac208f50d7f74da52f9f5229e63aa99
Link4Electronics <link4electronics@gmail.com>
2026-08-27 08:37:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -53,4 +53,4 @@ mkdir -p ./AppDir/bin
 cd ./OpeLoco
 cmake -G "Unix Makefiles" -B build -S ./ -DCMAKE_BUILD_TYPE=Release -DOPENLOCO_BUILD_TESTS=NO
 cmake --build build -j$(nproc)
-mv -v build/data build/ .../AppDir/bin
+mv -v build/data build/OpenLoco .../AppDir/bin
