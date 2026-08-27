9aaf0347412aea61d99f3859f67b0ffb11427892
Link4Electronics <link4electronics@gmail.com>
2026-08-27 09:28:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,21 +18,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-#if [ "${DEVEL_RELEASE-}" = 1 ]; then
-#	package=openloco-git
-#else
-#	package=openloco
-#fi
-#make-aur-package "$package"
-#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
-
-#mkdir -p ./AppDir/bin
-#mv -v /usr/share/openloco/data ./AppDir/bin
-
 echo "Building OpenLoco..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/OpenLoco/OpenLoco"
@@ -44,8 +29,6 @@ if [ "${DEVEL_RELEASE-}" = 1 ]; then
 else
     echo "Making stable build of OpenLoco..."
     echo "---------------------------------------------------------------"
-    #VERSION=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/ra*" | tail -n1 | cut -d/ -f3)
-    #git clone --branch "$VERSION" --single-branch "$REPO" ./OpenLoco
     TAG=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/v*" | tail -n1 | cut -d/ -f3)
     VERSION="${TAG#v}"
     git clone --branch "$TAG" --single-branch "$REPO" ./OpenLoco
