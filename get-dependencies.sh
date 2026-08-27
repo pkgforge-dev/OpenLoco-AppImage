9b01c5ead3595b0d09166f7e209b7105338628f2
Link4Electronics <link4electronics@gmail.com>
2026-08-27 08:31:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    cmake    \
+    fmt      \
+    onetbb \
+    openal   \
+    sdl3     \
+    yaml-cpp
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -29,16 +35,18 @@ get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Building OpenLoco..."
 echo "---------------------------------------------------------------"
-REPO="https://github.com/dalerank/Akhenaten"
+REPO="https://github.com/OpenLoco/OpenLoco"
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
     echo "Making nightly build of OpenLoco..."
     echo "---------------------------------------------------------------"
     VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-    git clone "$REPO" ./Akhenaten
+    git clone "$REPO" ./OpenLoco
 else
     echo "Making stable build of OpenLoco..."
     echo "---------------------------------------------------------------"
     VERSION=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/ra*" | tail -n1 | cut -d/ -f3)
-    git clone --branch "$VERSION" --single-branch "$REPO" ./Akhenaten
+    git clone --branch "$VERSION" --single-branch "$REPO" ./OpenLoco
 fi
 echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
