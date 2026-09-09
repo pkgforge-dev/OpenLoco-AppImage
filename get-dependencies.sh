e83f1dc92290062cf67ffd4f30b2936c53ab33b9
Link4Electronics <link4electronics@gmail.com>
2026-09-08 23:42:02 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -25,18 +25,18 @@ if [ "${DEVEL_RELEASE-}" = 1 ]; then
     echo "Making nightly build of OpenLoco..."
     echo "---------------------------------------------------------------"
     VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
-    git clone "$REPO" ./OpenLoco
+    git clone --depth 1 "$REPO" ./OpenLoco
 else
     echo "Making stable build of OpenLoco..."
     echo "---------------------------------------------------------------"
     TAG=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/v*" | tail -n1 | cut -d/ -f3)
     VERSION="${TAG#v}"
-    git clone --branch "$TAG" --single-branch "$REPO" ./OpenLoco
+    git clone --branch "$TAG" --single-branch --depth 1 "$REPO" ./OpenLoco
 fi
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./OpenLoco
-cmake -G "Unix Makefiles" -B build -S ./ -DCMAKE_BUILD_TYPE=Release -DOPENLOCO_BUILD_TESTS=NO
+cmake -G "Unix Makefiles" -B build ./ -DCMAKE_BUILD_TYPE=Release -DOPENLOCO_BUILD_TESTS=NO
 cmake --build build -j$(nproc)
 mv -v build/data build/OpenLoco ../AppDir/bin
